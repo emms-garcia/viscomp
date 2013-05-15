e60e5505fcb15265fe9dcf249a73b42838dd672d
Emmanuel <aleksgs10@gmail.com>
2013-05-15 17:25:55 -0500

    Eliminación de OpenCv, mismos resultados con Pygame + PIL

--- motion_lab.py
+++ motion_lab.py
@@ -1,106 +1,63 @@
 import Image
-import cv2.cv as cv
+import ImageDraw
 import numpy
 import sys
 import math
-import time
+import pygame
 
-c = cv.CreateCameraCapture(0)
-cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_WIDTH, 280 )
-cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_HEIGHT, 160 )
-VIDEOFILE = False
+pygame.init()
 
-try:
-  if ".avi" in sys.argv[1]:
-    VIDEOFILE = True
-    c = cv.CaptureFromFile( sys.argv[1] ) 
-    NFRAMES = cv.GetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_COUNT  )
-    WIDTH = int(cv.GetCaptureProperty(c, cv.CV_CAP_PROP_FRAME_WIDTH))
-    HEIGHT = int(cv.GetCaptureProperty(c, cv.CV_CAP_PROP_FRAME_HEIGHT))
-except:
-  print "Error, no se especifico un video de entrada."
-  sys.exit()
-
-def cv2array(im):
-  depth2dtype = {
-        cv.IPL_DEPTH_8U: 'uint8',
-        cv.IPL_DEPTH_8S: 'int8',
-        cv.IPL_DEPTH_16U: 'uint16',
-        cv.IPL_DEPTH_16S: 'int16',
-        cv.IPL_DEPTH_32S: 'int32',
-        cv.IPL_DEPTH_32F: 'float32',
-        cv.IPL_DEPTH_64F: 'float64',
-    }
-
-  arrdtype=im.depth
-  a = numpy.fromstring(
-         im.tostring(),
-         dtype=depth2dtype[im.depth],
-         count=im.width*im.height*im.nChannels)
-  a.shape = (im.height,im.width,im.nChannels)
-  return a
-
-def array2cv(a):
-  dtype2depth = {
-        'uint8':   cv.IPL_DEPTH_8U,
-        'int8':    cv.IPL_DEPTH_8S,
-        'uint16':  cv.IPL_DEPTH_16U,
-        'int16':   cv.IPL_DEPTH_16S,
-        'int32':   cv.IPL_DEPTH_32S,
-        'float32': cv.IPL_DEPTH_32F,
-        'float64': cv.IPL_DEPTH_64F,
-    }
-  try:
-    nChannels = a.shape[2]
-  except:
-    nChannels = 1
-  cv_im = cv.CreateImageHeader((a.shape[1],a.shape[0]),
-          dtype2depth[str(a.dtype)],
-          nChannels)
-  cv.SetData(cv_im, a.tostring(),
-             a.dtype.itemsize*nChannels*a.shape[1])
-  return cv_im
-
+WIDTH, HEIGHT = 280, 160
+size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(size)
 
 def total_occupied_pixels(image, pt1, pt2):
   x1, y1 = pt1
   x2, y2 = pt2
-  WHITE = numpy.array([255, 255, 255], dtype = image.dtype)
+  WHITE = numpy.array([255], dtype = image.dtype)
   RED = numpy.array([255, 0, 0], dtype = image.dtype)
   count = 0
-  tmp = image[y1: y2, x1: x2, :]
+  tmp = image[y1: y2, x1: x2]
+  #print tmp
   count = numpy.where(tmp == WHITE)
-  count = len(count[0])/10
-    #print pt1, pt2, count
+  count = len(count[0])
   return count
 
 def center(image, color):
   color = numpy.array(color)
-  x, y, _ = numpy.where(image == color)
+  x, y = numpy.where(image == color)
   n = len(x)
   return int(y.mean()), int(x.mean())
 
-def grid(image, motion, size = 15):
+def grid(motion, image, size = 15):
+  image = Image.fromarray(image)
+  draw = ImageDraw.Draw(image)
   divx = WIDTH / size
   divy = HEIGHT / size
   AREA = size*size
-  pixels = cv2array(motion).copy()
-  #centers = []
+  pixels = motion.copy()
+  color, w = None, 1
   for i in range(divx):
     for j in range(divy):
       pt1 = i*size, j*size
       pt2 = (i + 1)*size, (j + 1)*size
       n = total_occupied_pixels(pixels, pt1, pt2)
-      #print n
-      if n > AREA*0.00001:
-        cv.Rectangle(image, pt1, pt2, cv.CV_RGB(0, 255, 0), 2)
-        #centers.append((pt1[0] + size/2, pt1[1] + size/2))
+      Rect = (pt1[0], pt1[1], pt2[0], pt2[1])
+      if n > 0:
+        w = 2
+        color = (0, 255, 0)
       else: 
-        cv.Rectangle(image, pt1, pt2, cv.CV_RGB(128, 128, 128), 1)
-  #n = len(centers)
-  #centers = [sum(i) for i in zip(*centers)] 
-  #return centers[0]/n, centers[1]/n
-  return center(pixels, (255, 255, 255))
+        w = 1
+        color = (128, 128, 128)
+      #Draw rectangle no soporta cambio de ancho de linea
+      draw.line((pt1, (pt2[0], pt1[1])), color, width = w)
+      draw.line(((pt2[0], pt1[1]), pt2), color, width = w)
+      draw.line((pt2, (pt1[0], pt2[1])), color, width = w)
+      draw.line(((pt1[0], pt2[1]), pt1), color, width = w)
+        #draw.rectangle(Rect, outline = (128, 128, 128))
+  pygame.display.update()
+  image.save('tmp.png', 'PNG')
+  return center(pixels, (255))
 
 def grayscale(image):
   w, h, _ = image.shape
@@ -138,32 +95,35 @@ def direction(p, q, last):
 current_frame = 0
 movement = "Nothing"
 frames = []
-while True:
-  if VIDEOFILE: cv.SetCaptureProperty(c, cv.CV_CAP_PROP_POS_FRAMES, current_frame )
-  camera_image = cv.QueryFrame( c )
-  frame1 = cv.CloneImage( camera_image )
-  array1 = cv2array(frame1)
 
-  camera_image = cv.QueryFrame( c )
-  frame2 = cv.CloneImage( camera_image )
-  array2 = cv2array(frame2)
+clock = pygame.time.Clock()
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: sys.exit()
+  
+  frame1 = Image.open('frames/output_frame_%d.png'%current_frame)
+  current_frame += 1
+  array1 = numpy.array(frame1)
+
+  frame2 = Image.open('frames/output_frame_%d.png'%current_frame)
+  array2 = numpy.array(frame2)
+  current_frame += 1
 
   out = difference(array1, array2)
   out = grayscale(out)
   motion = threshold(out, 15)
-
-  motion = array2cv(out)
-  cent = grid(frame1, motion)
+  cent = grid(motion, array2)
   frames.append(cent)
 
   if len(frames) == 2:
     direction(frames[0], frames[1], movement)
     frames.pop(0)
 
-  cv.ShowImage('e2', frame1)
-  current_frame += 1
-  if current_frame >= NFRAMES - 1: 
+  background = pygame.image.load("tmp.png")
+  backgroundRect = background.get_rect()
+  screen.blit(background, backgroundRect)
+  pygame.display.update()
+
+  if current_frame >= 200: 
     break
 
-  if cv.WaitKey(5) == 27:
-    break
