01197b25ef89c5ffa39d958d0e551cf46423a0cd
Emmanuel <aleksgs10@gmail.com>
2013-05-15 16:00:31 -0500

    Direccion de movimiento detectada

--- generate_motion.py
+++ generate_motion.py
@@ -3,36 +3,41 @@ import random
 import numpy
 import sys
 import os
+import time
 
 WIDTH, HEIGHT = 280, 160
 bg = numpy.empty((HEIGHT, WIDTH))
 bg.fill(255)
 bg = Image.fromarray(bg).convert('RGB')
+os.system("rm jpgs/*")
 
-def draw_square(image, (x, y), size, color, n):
+def draw_square(image, (x, y), k, color, n):
   w, h = image.size
   pix = image.load()
-  size = [k for k in range(-size/2 + 1, size/2 + 1)]
-  for i in size:
-    for j in size:
+  k = [k for k in range(-k/2 + 1, k/2 + 1)]
+  for i in k:
+    for j in k:
       dx, dy = x + i, y + j
       pix[dx, dy] = color
   image.save('jpgs/output_frame_%s.png'%n, 'PNG')
   return image
 
 points = []
-size = 21
+size = 20
+speed = size/2
 color = (255, 0, 0)
 num = 0
-x1, y1 = random.randint(0+size, WIDTH-size), random.randint(0+size, HEIGHT-size)
-for i in range(int(sys.argv[1])):
-  x2, y2 = random.randint(0+size, WIDTH-size), random.randint(0+size, HEIGHT-size)
+x1, y1 = random.randrange(0+size, WIDTH-size, speed), random.randrange(0+size, HEIGHT-size, speed)
+INICIO = time.time()
+t = 0
+while t < float(sys.argv[1])/4:
+  x2, y2 = random.randrange(0+size, WIDTH-size, size), random.randrange(0+size, HEIGHT-size, size)
   if x1 > x2:
     while x1 is not x2:
       frame = bg.copy()
       frame = draw_square(frame, (x1, y1), size, color, num)
       num += 1
-      x1 -= 1 
+      x1 -= speed
       if x1 == x2:
         break
       
@@ -41,7 +46,7 @@ for i in range(int(sys.argv[1])):
       frame = bg.copy()
       frame = draw_square(frame, (x1, y1), size, color, num)
       num += 1
-      x1 += 1
+      x1 += speed
       if x1 == x2:
         break
   if y1 > y2:
@@ -49,7 +54,7 @@ for i in range(int(sys.argv[1])):
       frame = bg.copy()
       frame = draw_square(frame, (x1, y1), size, color, num)
       num += 1
-      y1 -= 1
+      y1 -= speed
       if y1 == y2:
         break
   elif y1 < y2:
@@ -57,11 +62,13 @@ for i in range(int(sys.argv[1])):
       frame = bg.copy()
       frame = draw_square(frame, (x1, y1), size, color, num)
       num += 1
-      y1 += 1
+      y1 += speed
       if y1 == y2:
         break
   x1, y1 = x2, y2
+  ACTUAL = time.time()
+  t = ACTUAL - INICIO
 
 os.system("avconv -f image2 -i jpgs/output_frame_%d.png videos/tmp.avi")
-os.system("mencoder videos/out.avi -ovc raw -vf format=i420 -nosound -o videos/out.avi")
+os.system("mencoder videos/tmp.avi -ovc raw -vf format=i420 -nosound -o videos/out.avi")
 os.system("rm videos/tmp.avi")
--- motion_lab.py
+++ motion_lab.py
@@ -2,11 +2,14 @@ import Image
 import cv2.cv as cv
 import numpy
 import sys
+import math
+import time
 
 c = cv.CreateCameraCapture(0)
 cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_WIDTH, 280 )
 cv.SetCaptureProperty( c, cv.CV_CAP_PROP_FRAME_HEIGHT, 160 )
 VIDEOFILE = False
+
 try:
   if ".avi" in sys.argv[1]:
     VIDEOFILE = True
@@ -15,8 +18,9 @@ try:
     WIDTH = int(cv.GetCaptureProperty(c, cv.CV_CAP_PROP_FRAME_WIDTH))
     HEIGHT = int(cv.GetCaptureProperty(c, cv.CV_CAP_PROP_FRAME_HEIGHT))
 except:
-  print "No se especifico video de entrada, usando camara web."
-  print "Usa %s -h   o %s --help para conocer los parametros."%(sys.argv[0], sys.argv[0])
+  print "Error, no se especifico un video de entrada."
+  sys.exit()
+
 def cv2array(im):
   depth2dtype = {
         cv.IPL_DEPTH_8U: 'uint8',
@@ -56,59 +60,7 @@ def array2cv(a):
   cv.SetData(cv_im, a.tostring(),
              a.dtype.itemsize*nChannels*a.shape[1])
   return cv_im
-def cv2array(im):
-  depth2dtype = {
-        cv.IPL_DEPTH_8U: 'uint8',
-        cv.IPL_DEPTH_8S: 'int8',
-        cv.IPL_DEPTH_16U: 'uint16',
-        cv.IPL_DEPTH_16S: 'int16',
-        cv.IPL_DEPTH_32S: 'int32',
-        cv.IPL_DEPTH_32F: 'float32',
-        cv.IPL_DEPTH_64F: 'float64',
-    }
 
-  arrdtype=im.depth
-  a = numpy.fromstring(
-         im.tostring(),
-         dtype=depth2dtype[im.depth],
-         count=im.width*im.height*im.nChannels)
-  a.shape = (im.height,im.width,im.nChannels)
-  return a
-
-def array2cv(a):
-  dtype2depth = {
-        'uint8':   cv.IPL_DEPTH_8U,
-        'int8':    cv.IPL_DEPTH_8S,
-        'uint16':  cv.IPL_DEPTH_16U,
-        'int16':   cv.IPL_DEPTH_16S,
-        'int32':   cv.IPL_DEPTH_32S,
-        'float32': cv.IPL_DEPTH_32F,
-        'float64': cv.IPL_DEPTH_64F,
-    }
-  try:
-    nChannels = a.shape[2]
-  except:
-    nChannels = 1
-  cv_im = cv.CreateImageHeader((a.shape[1],a.shape[0]),
-          dtype2depth[str(a.dtype)],
-          nChannels)
-  cv.SetData(cv_im, a.tostring(),
-             a.dtype.itemsize*nChannels*a.shape[1])
-  return cv_im
-
-#def threshold(image, umb):
-#  umb = numpy.empty(image.shape, dtype = image.dtype).fill(umb)
-#  WHITE = numpy.array(255)
-#  BLACK = numpy.array(0)
-#  out = image.copy()
-#  w, h = image.shape
-#  for i in range(w):
-#    for j in range(h):
-#      if image[i, j] >= umb:
-#        out[i, j] = WHITE
-#      else:
-#        out[i, j] = BLACK
-#  return out
 
 def total_occupied_pixels(image, pt1, pt2):
   x1, y1 = pt1
@@ -117,34 +69,50 @@ def total_occupied_pixels(image, pt1, pt2):
   RED = numpy.array([255, 0, 0], dtype = image.dtype)
   count = 0
   tmp = image[y1: y2, x1: x2, :]
-  count = numpy.where(tmp != WHITE)
+  count = numpy.where(tmp == WHITE)
   count = len(count[0])/10
     #print pt1, pt2, count
   return count
 
-def grid(image, size = 10):
+def center(image, color):
+  color = numpy.array(color)
+  x, y, _ = numpy.where(image == color)
+  n = len(x)
+  return int(y.mean()), int(x.mean())
+
+def grid(image, motion, size = 15):
   divx = WIDTH / size
   divy = HEIGHT / size
   AREA = size*size
-  pixels = cv2array(image).copy()
+  pixels = cv2array(motion).copy()
+  #centers = []
   for i in range(divx):
     for j in range(divy):
       pt1 = i*size, j*size
       pt2 = (i + 1)*size, (j + 1)*size
       n = total_occupied_pixels(pixels, pt1, pt2)
       #print n
-      if n > AREA*0.07:
+      if n > AREA*0.00001:
         cv.Rectangle(image, pt1, pt2, cv.CV_RGB(0, 255, 0), 2)
+        #centers.append((pt1[0] + size/2, pt1[1] + size/2))
       else: 
         cv.Rectangle(image, pt1, pt2, cv.CV_RGB(128, 128, 128), 1)
-  return
-
+  #n = len(centers)
+  #centers = [sum(i) for i in zip(*centers)] 
+  #return centers[0]/n, centers[1]/n
+  return center(pixels, (255, 255, 255))
 
 def grayscale(image):
   w, h, _ = image.shape
   gray = numpy.sum(image.astype(numpy.int), axis=2) / 3
   gray = numpy.array(gray, dtype = image.dtype)
   return gray
+
+def threshold(image, umb):
+  umb = numpy.array([umb], dtype = image.dtype)
+  WHITE = numpy.array([255], dtype = image.dtype)
+  out[numpy.where(image > umb)] = WHITE
+  return out
 
 def difference(im1, im2):
   a = im1.astype(numpy.int)
@@ -153,37 +121,48 @@ def difference(im1, im2):
   diff = diff.astype(numpy.uint8)
   return diff
 
-#def search_color(image, color):
-#  return numpy.where(image == numpy.array(color))[0]
+def direction(p, q, last):
+  x1, y1 = p
+  x2, y2 = q
+  angle = math.atan2(p[1] - q[1], p[0] - q[0])
+  if int(math.degrees(angle)) in range(135, 225):
+    print "Derecha, ",math.degrees(angle)
+  elif int(math.degrees(angle)) in range(-45, 46):
+    print "Izquierda, ", math.degrees(angle)
+  elif int(math.degrees(angle)) in range(45, 136):
+    print "Arriba, ", math.degrees(angle)
+  else:
+    print 'Abajo', math.degrees(angle)
+  return
 
 current_frame = 0
+movement = "Nothing"
+frames = []
 while True:
   if VIDEOFILE: cv.SetCaptureProperty(c, cv.CV_CAP_PROP_POS_FRAMES, current_frame )
   camera_image = cv.QueryFrame( c )
   frame1 = cv.CloneImage( camera_image )
-#  cv.ConvertImage(frame1, frame1, cv.CV_CVTIMG_FLIP)
   array1 = cv2array(frame1)
-  #pil1 = smooth(pil1)
 
   camera_image = cv.QueryFrame( c )
   frame2 = cv.CloneImage( camera_image )
-#  cv.ConvertImage(frame2, frame2, cv.CV_CVTIMG_FLIP)
   array2 = cv2array(frame2)
-#  #pil2 = smooth(pil2)
 
-#  out = difference(array1, array2)
-#  out = grayscale(out)
-#  out = threshold(out, 2)
-#  print search_color(out, (0, 0, 0))
-  
-#  display_image = array2cv(out)
-  display_image = frame2
-  grid(display_image)
-  cv.ShowImage('e2', display_image)
-  
+  out = difference(array1, array2)
+  out = grayscale(out)
+  motion = threshold(out, 15)
+
+  motion = array2cv(out)
+  cent = grid(frame1, motion)
+  frames.append(cent)
+
+  if len(frames) == 2:
+    direction(frames[0], frames[1], movement)
+    frames.pop(0)
+
+  cv.ShowImage('e2', frame1)
   current_frame += 1
-
-  if current_frame == NFRAMES - 1:
+  if current_frame >= NFRAMES - 1: 
     break
 
   if cv.WaitKey(5) == 27:
